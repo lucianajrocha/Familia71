eec25f81eae1b998747144bea72e79e3df9a1b0d
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-06 14:33:56 -0300

    feat: implementando o exercicio 3

--- src/ExerciciosVariaveisOperadores/Exercicio3.java
+++ src/ExerciciosVariaveisOperadores/Exercicio3.java
@@ -1,5 +1,30 @@
 package ExerciciosVariaveisOperadores;
 
-public class Exercicio3 {
+import java.util.Scanner;
 
+public class Exercicio3 {
+	
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in); 
+
+		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+		
+		System.out.println("Digite o salário bruto: ");
+		salarioBruto = leia.nextFloat();
+		
+		System.out.println("Digite o adicional noturno: ");
+		adicionalNoturno = leia.nextFloat();
+		
+		System.out.println("Digite as horas extras: ");
+		horasExtras = leia.nextFloat();
+		
+		System.out.println("Digite os descontos: ");
+		descontos = leia.nextFloat();
+		
+		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+		
+		System.out.println("O salário líquido é: " + salarioLiquido);
+	}
 }
+
