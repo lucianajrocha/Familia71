bbd14d4bcb585a1bc71baf5769f14714bb0b68d6
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-06 15:00:59 -0300

    feat: implementando o exercicio 4

--- src/ExerciciosVariaveisOperadores/Exercicio4.java
+++ src/ExerciciosVariaveisOperadores/Exercicio4.java
@@ -1,5 +1,33 @@
 package ExerciciosVariaveisOperadores;
 
-public class Exercicio4 {
+import java.util.Scanner;
 
+public class Exercicio4 {
+	
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in); 
+
+		float n1, n2, n3, n4, resultado;
+		
+		System.out.println("Digite o número 1: ");
+		n1 = leia.nextFloat();
+		
+		System.out.println("Digite o número 2: ");
+		n2 = leia.nextFloat();
+		
+		System.out.println("Digite o número 3: ");
+		n3 = leia.nextFloat();
+		
+		System.out.println("Digite o número 4: ");
+		n4 = leia.nextFloat();
+		
+		resultado = (n1 * n2) - (n3 * n4);
+		
+		System.out.println("Diferença: " + resultado);
+		
+		
+		
+
+	}
 }
