bc0d50182d99623cf23859d1c77e828d57ea3351
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-08 11:59:21 -0300

    feat:implemento do exercicio3 da lista do dia 7/12/23

--- src/DeclaracoesCondicionais/Exercicio3.java
+++ src/DeclaracoesCondicionais/Exercicio3.java
@@ -5,39 +5,35 @@ import java.util.Scanner;
 public class Exercicio3 {
 
 	public static void main(String[] args) {
-		Scanner leitor = new Scanner (System.in);
-		
+		Scanner leitor = new Scanner(System.in);
+
 		int idade;
 		String nome;
 		boolean primeiraDoacao;
-		
-		
-		
-		// entre 18 e 69 anos
-		// 60 e 69 só podem doar se não for sua primeira doação
 
-		System.out.println("Digite o Nome do doador:");
+
+		System.out.print("Digite o Nome do doador: ");
 		nome = leitor.nextLine();
-		System.out.println("Digite a Idade do doador:");
+		System.out.print("Digite a Idade do doador: ");
 		idade = leitor.nextInt();
-		System.out.println("Primeira doação de sangue?");
+		System.out.print("Primeira doação de sangue? ");
 		primeiraDoacao = leitor.nextBoolean();
-		
-		
-		if(idade >=60 && idade <=69 && true) {
-			System.out.println(nome + "não está apto para doar sangue!");
-			} 
-		if(idade >=18 && idade <=69 && true) {
-			System.out.println(nome + " está apto para doar sangue!");
-		} 
-		if (idade <= 18 && true)
-		  {
-				System.out.println(nome + " não está apta para doar sangue");
-			} 
-		
-	
-			
+
+		if (idade >= 18 && idade <= 69) {
+			if (idade >= 60 && idade <= 69) {
+				if (primeiraDoacao) {
+					System.out.println(nome + " não está apto para doar sangue!");
+				} else {
+					System.out.println(nome + " está apto para doar sangue!");
+				}
+
+			} else {
+				System.out.println(nome + " está apto para doar sangue!");
+			}
+
+		} else {
+			System.out.println(nome + " não está apto para doar sangue!");
 		}
 	}
 
-
+}
