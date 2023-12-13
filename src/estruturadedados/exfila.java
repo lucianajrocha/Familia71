d763984a0128c077e9674f9913173be7ae22db23
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-13 13:55:26 -0300

    feat: exercicios de pilha e fila finalizados

--- src/estruturadedados/exfila.java
+++ src/estruturadedados/exfila.java
@@ -1,7 +1,68 @@
 package estruturadedados;
 
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class exfila {
     public static void main(String[] args) {
+        Queue<String> clienteBanco = new LinkedList<String>();
+        Scanner ler = new Scanner(System.in);
+        int op;
+
+        do {
+            System.out.println("\n***************************");
+            System.out.println("\n(1) Adicionar Cliente na Fila");
+            System.out.println("\n(2) Listar todos os Clientes");
+            System.out.println("\n(3) Retirar Cliente da Fila");
+            System.out.println("\n(0) Sair");
+            System.out.println("\n***************************");
+            System.out.println("\n Entre com a opção desejada: ");
+
+            op = ler.nextInt();
+
+            switch (op) {
+
+                case 1:
+                    ler.nextLine();
+                    System.out.println("Digite um nome: ");
+                    clienteBanco.add(ler.nextLine());
+                    System.out.println("Cliente adicionado!");
+                    break;
+
+                case 2:
+                    System.out.println("Lista de Clientes na Fila:");
+                    System.out.println(clienteBanco);
+                    break;
+
+                case 3:
+                    ler.nextLine();
+                    if (clienteBanco.isEmpty()) {
+                        System.out.println("A Fila está vazia!\n");
+                    } else {
+
+                        String remove = clienteBanco.remove();
+                        System.out.println("\nFila: " + "\n Nome:");
+                        System.out.println(clienteBanco);
+                        System.out.println(("\nO cliente foi chamado!"));
+
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Programa Finalizado!");
+
+                default:
+                    break;
+
+
+            }
+
+
+        } while (op != 0);
+
 
     }
 }
+
--- src/estruturadedados/expilha.java
+++ src/estruturadedados/expilha.java
@@ -1,7 +1,60 @@
 package estruturadedados;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class expilha {
     public static void main(String[] args) {
+        Stack<String> pilhaLivro = new Stack<String>();
+        Scanner ler = new Scanner(System.in);
+        int op;
 
+        do {
+            System.out.println("\n***************************");
+            System.out.println("\n(1) Adicionar Livro na Pilha");
+            System.out.println("\n(2) Listar todos os Livros");
+            System.out.println("\n(3) Retirar Livro da Pilha");
+            System.out.println("\n(0) Sair");
+            System.out.println("\n***************************");
+            System.out.println("\n Entre com a opção desejada: ");
+
+            op = ler.nextInt();
+
+            switch (op) {
+
+                case 1:
+                    ler.nextLine();
+                    System.out.println("Digite o nome: ");
+                    pilhaLivro.push(ler.nextLine());
+                    System.out.println("Livro adicionado!");
+                    break;
+
+                case 2:
+                    System.out.println("Lista de Livros na Pilha:");
+                    System.out.println(pilhaLivro);
+                    break;
+
+                case 3:
+                    ler.nextLine();
+                    if (pilhaLivro.empty()) {
+                        System.out.println("A Pilha está vazia!\n");
+                    } else {
+                        String remove = pilhaLivro.pop();
+                        System.out.println("\nPilha: ");
+                        System.out.println(pilhaLivro);
+                        System.out.println(("\nUm Livro foi retirado da pilha!"));
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Programa Finalizado!");
+
+                default:
+                    break;
+
+
+            }
+        } while (op != 0);
     }
 }
+
